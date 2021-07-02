e0aa373c8637f5e086abfb22a9f2f3197a04fdef
Wellington <wellington@fetch.ly>
2021-07-02 10:05:06 -0300

    add get_sales_tax_rates_by_address from TaxService

--- lib/ship_compliant/tax_service.rb
+++ lib/ship_compliant/tax_service.rb
@@ -10,5 +10,10 @@ module ShipCompliant
       result = ShipCompliant.client.call(:get_tax_rate, camel_cased_keys)
     end
 
+    def self.get_sales_tax_rates_by_address(data)
+      camel_cased_keys = data.deep_transform_keys { |key| key.to_s.camelize }
+      result = ShipCompliant.client.call(:get_sales_tax_rates_by_address, camel_cased_keys)
+    end
+
   end
 end
